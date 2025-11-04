6a0f80d5dedf5a1eb7e6550bf7a32593f78902e4
zerochae <zerochae@kakao.com>
2025-11-04 16:49:20 +0900

    style: widget.sh 인덴트 및 아이콘 순서 정리
    - 인덴트를 4칸에서 2칸으로 변경
    - 아이콘 순서 재정렬 (관련 아이콘끼리 그룹화)

--- icons/widget.sh
+++ icons/widget.sh
@@ -2,114 +2,114 @@
 
 ### START-OF-WIDGET-ICON-MAP
 function __widget_icon_map() {
-    case "$1" in
-    "block-1")
-        icon_result="▁"
-        ;;
+  case "$1" in
+  "block-1")
+    icon_result="▁"
+    ;;
 
-    "block-2")
-        icon_result="▂"
-        ;;
+  "block-2")
+    icon_result="▂"
+    ;;
 
-    "block-3")
-        icon_result="▃"
-        ;;
+  "block-3")
+    icon_result="▃"
+    ;;
 
-    "block-4")
-        icon_result="▄"
-        ;;
+  "block-4")
+    icon_result="▄"
+    ;;
 
-    "block-5")
-        icon_result="▅"
-        ;;
+  "block-5")
+    icon_result="▅"
+    ;;
 
-    "block-6")
-        icon_result="▆"
-        ;;
+  "block-6")
+    icon_result="▆"
+    ;;
 
-    "block-7")
-        icon_result="▇"
-        ;;
+  "block-7")
+    icon_result="▇"
+    ;;
 
-    "block-8")
-        icon_result="█"
-        ;;
+  "block-8")
+    icon_result="█"
+    ;;
 
-    "battery_empty")
-        icon_result=""
-        ;;
+  "battery_empty")
+    icon_result=""
+    ;;
 
-    "battery_low")
-        icon_result=""
-        ;;
+  "battery_low")
+    icon_result=""
+    ;;
 
-    "battery_medium")
-        icon_result=""
-        ;;
+  "battery_medium")
+    icon_result=""
+    ;;
 
-    "battery_full")
-        icon_result=""
-        ;;
+  "battery_full")
+    icon_result=""
+    ;;
 
-    "clock")
-        icon_result=""
-        ;;
+  "clock")
+    icon_result=""
+    ;;
 
-    "network_download")
-        icon_result=""
-        ;;
+  "network_download")
+    icon_result=""
+    ;;
 
-    "network_upload")
-        icon_result=""
-        ;;
+  "network_upload")
+    icon_result=""
+    ;;
 
-    "disk")
-        icon_result=""
-        ;;
+  "disk")
+    icon_result=""
+    ;;
 
-    "loading")
-        icon_result=""
-        ;;
+  "loading")
+    icon_result=""
+    ;;
 
-    "volume_medium")
-        icon_result=""
-        ;;
+  "volume_medium")
+    icon_result=""
+    ;;
 
-    "volume_low")
-        icon_result=""
-        ;;
+  "volume_low")
+    icon_result=""
+    ;;
 
-    "volume_high")
-        icon_result=""
-        ;;
+  "volume_high")
+    icon_result=""
+    ;;
 
-    "cpu")
-        icon_result=""
-        ;;
+  "volume_mute")
+    icon_result="󰝟"
+    ;;
 
-    "coffee_on")
-        icon_result="󰅶"
-        ;;
+  "cpu")
+    icon_result=""
+    ;;
 
-    "memory")
-        icon_result="󰍛"
-        ;;
+  "coffee_on")
+    icon_result="󰅶"
+    ;;
 
-    "coffee_off")
-        icon_result="󰶞"
-        ;;
+  "coffee_off")
+    icon_result="󰶞"
+    ;;
 
-    "volume_mute")
-        icon_result="󰝟"
-        ;;
+  "memory")
+    icon_result="󰍛"
+    ;;
 
-    "config")
-        icon_result=""
-        ;;
+  "config")
+    icon_result=""
+    ;;
 
-    *)
-        icon_result=""
-        ;;
-    esac
+  *)
+    icon_result=""
+    ;;
+  esac
 }
 ### END-OF-WIDGET-ICON-MAP
